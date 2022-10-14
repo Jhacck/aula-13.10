1703d2cb88b8814c1b60a167ee5837e8f1fc82a8
Jaqueline Silva <jhacck25@gmail.com>
2022-10-13 21:56:15 -0300

    Aula 13/10

---
+++ index.js
@@ -0,0 +1,88 @@
+//Aula para calcular comprimento da circunferência (Operadores aritméticos)
+const pi = 3.1415;
+let r = 8;
+//console.log(`Comprimento da circunferência ${2*pi*r}`);
+
+//Operadores Relacionais 
+/* 
+== Igual
+!= Diferente
+< Menor
+> Maior 
+<= Menor igual
+>= Maior igual
+*/
+
+let x = 10;
+let y = 3;
+/*
+console.log(x == y);
+console.log(x != y);
+console.log( x > y);
+console.log( x < y);
+console.log( x >= y);
+console.log( x <= y);
+*/
+
+//Frases diferentes 
+let um = "Gato praticando programação";
+let dois = "Cachorro praticando análise de dados";
+/*
+console.log(um == dois);
+console.log(um != dois);
+*/
+
+//Frases Iguais utilizando string
+let tres = "Gato praticando programação";
+let quatro = "Gato praticando programação";
+/*console.log(tres == quatro);
+console.log(tres != quatro);*/
+
+//Números - utilizando 
+let numero1 = 10;
+let numero2 = 5;
+/*
+console.log(numero1 == numero2);
+console.log(numero1 === numero2);
+console.log(numero1 != numero2);
+console.log(numero1 >= numero2);
+console.log(numero1 <= numero2);
+console.log(numero1 > numero2);
+console.log(numero1 < numero2);
+*/
+
+//Operadores Condicionais
+//SE(if), SENÃO(else),
+let a = "choveu";
+let b = "fez sol";
+let c = "nublado;"
+
+/*
+if (b == "choveu") {
+    console.log("Hoje choveu demais!!");
+} else if (c == "nublado") {
+    console.log("Está nublado hoje!");
+} else {
+    console.log("Hoje não choveu!");
+}
+*/
+
+let d = "Chocolate"
+let e = "Uva"
+let f = "Morango"
+
+if (e == "Chocolate"){
+    console.log("Gosto de milkshake de chocolate");
+} else {
+    console.log("Não gosto de milkshake!");
+}
+
+if (d == "Uva"){
+    console.log("Gosto de suco de uva!");
+} else if (d == "Morango"){
+    console.log("Gosto de suco de morango!")
+}else {
+    console.log("Não quero beber nada, obrigada!");
+}
+
+
